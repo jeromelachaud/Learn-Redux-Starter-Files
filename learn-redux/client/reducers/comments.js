76d16addf575c05bcd4e9ef245d4de6b15c5ee06
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-07-02 18:49:42 +0100

    wip

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,9 @@
+//  a reducers takes in two things:
+
+// 1. the action (info about what happened)
+// 2. copy of current state
+
+function comments(state = [], action) {
+  return state;
+}
+export default comments;
